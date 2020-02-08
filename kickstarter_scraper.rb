02befa2be18b2a08b0c8398cd37b8432741a1841
Ian Marrs <ian.t.marrs@gmail.com>
2020-02-08 08:21:30 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,9 +5,8 @@ def create_project_hash
   # projects: kickstarter.css("li.project.grid_4")
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  
+  binding.pry
 end
 
 
 create_project_hash
-binding.pry
